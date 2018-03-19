e6fc9e07d8cbb1e2aaad020cc4cd42d7d10f45c0
ayenpure <ayenpure@uoregon.edu>
2018-03-19 13:57:44 -0700

    Adding reverted Sentiment mapper

--- src/stockmeup/processing/SentimentMapper.java
+++ src/stockmeup/processing/SentimentMapper.java
@@ -6,32 +6,32 @@ import java.util.Properties;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Mapper;
 
-/*import edu.stanford.nlp.ling.CoreAnnotations;
+import edu.stanford.nlp.ling.CoreAnnotations;
 import edu.stanford.nlp.neural.rnn.RNNCoreAnnotations;
 import edu.stanford.nlp.pipeline.Annotation;
 import edu.stanford.nlp.pipeline.StanfordCoreNLP;
 import edu.stanford.nlp.sentiment.SentimentCoreAnnotations;
 import edu.stanford.nlp.trees.Tree;
-import edu.stanford.nlp.util.CoreMap;*/
+import edu.stanford.nlp.util.CoreMap;
 import stockmeup.miner.AuxiliaryFunctions;
 import stockmeup.miner.MinedObject;
 
 public class SentimentMapper extends Mapper<Object, Text, Text, Integer> {
 
-//	private static StanfordCoreNLP pipeline;
+	private static StanfordCoreNLP pipeline;
 
-/*	public static void setupPipeline() {
+	public static void setupPipeline() {
 		Properties props = new Properties();
 		props.setProperty("annotators", "tokenize, ssplit, parse, sentiment");
 		pipeline = new StanfordCoreNLP(props);
-	}*/
+	}
 
 	@Override
 	protected void map(Object key, Text value, Mapper<Object, Text, Text, Integer>.Context context)
 			throws IOException, InterruptedException {
 		System.out.println("Processing key : " + key);
 		System.out.println("Processing key : " + value);
-		/*int sentimentScore = 0, longest = 0;
+		int sentimentScore = 0, longest = 0;
 		String trackerinfo = value.toString();
 		String[] split = trackerinfo.split("\\s+");
 		String content = "";
@@ -53,7 +53,7 @@ public class SentimentMapper extends Mapper<Object, Text, Text, Integer> {
 				System.out.println("Main sentiment" + sentimentScore);
 				longest = partText.length();
 			}
-		}*/
-		context.write(new Text(value), value.toString().length());
+		}
+		context.write(new Text(split[0]), sentimentScore);
 	}
 }
--- src/stockmeup/testing/NLPTest.java
+++ src/stockmeup/testing/NLPTest.java
@@ -23,18 +23,18 @@ import stockmeup.miner.NewsMiner;
 
 public class NLPTest {
 
-	public static int averageList(List<Integer> list) { 
+	public static int averageList(List<Integer> list) {
 		int sum = 0;
-		for(Integer integer : list) {
+		for (Integer integer : list) {
 			sum += integer;
 		}
-		return sum / list.size(); 
+		return sum / list.size();
 	}
-	
+
 	public static void main(String[] args) throws FileNotFoundException, IOException {
-		
+
 		Map<String, List<MinedObject>> mining = new HashMap<String, List<MinedObject>>();
-		
+
 		File configDirectory = new File("/home/abhishek/repositories/StockMeUp/config/properties");
 		if (!configDirectory.isDirectory())
 			throw new RuntimeException("Given Path is not a Directory");
@@ -43,30 +43,30 @@ public class NLPTest {
 			Properties prop = new Properties();
 			prop.load(new FileInputStream(configFile));
 			for (Entry entry : prop.entrySet()) {
-//				System.out.println("**************************************" + entry.getValue());
+				// System.out.println("**************************************" +
+				// entry.getValue());
 				List<MinedObject> minedObjects = NewsMiner.extractForSource((String) entry.getValue());
-//				for (MinedObject minedObject : minedObjects) {
-//					System.out.println("----------------------------------------------------");
-//					System.out.println(minedObject.getTitle() + " : " + minedObject.getDate());
-//					System.out.println(minedObject.getDescription());
-//					System.out.println("----------------------------------------------------");
-//					System.out.println(AuxiliaryFunctions.toString(minedObject));
-//				}
+				// for (MinedObject minedObject : minedObjects) {
+				// System.out.println("----------------------------------------------------");
+				// System.out.println(minedObject.getTitle() + " : " + minedObject.getDate());
+				// System.out.println(minedObject.getDescription());
+				// System.out.println("----------------------------------------------------");
+				// System.out.println(AuxiliaryFunctions.toString(minedObject));
+				// }
 				mining.put((String) entry.getValue(), minedObjects);
 			}
 		}
-		
+
 		Properties props = new Properties();
 		props.setProperty("annotators", "tokenize, ssplit, parse, sentiment");
 		StanfordCoreNLP pipeline = new StanfordCoreNLP(props);
 
 		Map<String, List<Integer>> processing = new HashMap<String, List<Integer>>();
-		for(Entry<String, List<MinedObject>> entry : mining.entrySet())
-		{
+		for (Entry<String, List<MinedObject>> entry : mining.entrySet()) {
 			String tracker = entry.getKey();
 			List<MinedObject> minedObjects = entry.getValue();
 			List<Integer> minedSentiment = new ArrayList<Integer>();
-			for(MinedObject minedObject : minedObjects) {
+			for (MinedObject minedObject : minedObjects) {
 				int sentimentScore = 0;
 				String line = minedObject.getTitle();
 				if (line != null && line.length() > 0) {
@@ -78,7 +78,7 @@ public class NLPTest {
 						String partText = sentence.toString();
 						if (partText.length() > longest) {
 							sentimentScore = sentiment;
-							System.out.println("sentiment :" + sentimentScore + " for " + line);
+							System.out.println("sentiment :" + sentimentScore + " for " + "\"" + line + "\"");
 							longest = partText.length();
 						}
 					}
@@ -87,9 +87,8 @@ public class NLPTest {
 			}
 			processing.put(tracker, minedSentiment);
 		}
-		
-		for(Entry<String, List<Integer>> entry : processing.entrySet())
-		{
+
+		for (Entry<String, List<Integer>> entry : processing.entrySet()) {
 			String tracker = entry.getKey();
 			List<Integer> minedSentiment = entry.getValue();
 			System.out.println(tracker + ":" + averageList(minedSentiment));
